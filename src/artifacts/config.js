1feff3f2ebf189fa526363697112bc00bc919530
Alleo Indong <alleo.indong@gmail.com>
2022-02-17 18:52:56 +0800

    updated premint address

--- src/artifacts/config.js
+++ src/artifacts/config.js
@@ -1,5 +1,5 @@
 const config = {
-  preminterAddress: '0x4D50Afbb6B6177B20b44D17Dc857d7cf2e0a35Ce',
+  preminterAddress: '0x912fa2Af01E7596b7F848B3801d844855F3F352F',
   characterAddress: '0x2DCCb7fe1a5FD069202E6FA9cd1545652Ee2e87F',
   usdtAddress: '0x3813e82e6f7098b9583fc0f33a962d02018b6803',
 };
