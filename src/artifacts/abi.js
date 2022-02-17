ce9d70ad092c2d61d3342afffbf351bc8b91861e
Alleo Indong <alleo.indong@gmail.com>
2022-02-17 17:13:15 +0800

    added address config and abi on artifacts

---
+++ src/artifacts/abi.js
@@ -0,0 +1,18 @@
+export const premintABI = [
+  'function buyNewCharacter(uint256 qty) public returns (bool)',
+  'function getTotalCharactersMinted() public view returns (uint256)',
+  'event NewCharacterMinted(address indexed owner)',
+  'event CharacterPriceChanged(uint256 newPrice)',
+  'uint256 public characterNFTPrice',
+];
+
+export const characterABI = [
+  'function setApprovalForAll(address operator, bool approved) public virtual override',
+  'function isApprovedForAll(address owner, address operator) public view virtual override returns (bool)',
+  'event NewBlockHeadCreated(address indexed owner, uint256 indexed tokenId)',
+];
+
+export const erc20ABI = [
+  'function approve(address spender, uint256 amount) external returns (bool)',
+  'function allowance(address owner, address spender) external view returns (uint256)',
+];
---
+++ src/artifacts/config.js
@@ -0,0 +1,7 @@
+const config = {
+  preminterAddress: '0x4D50Afbb6B6177B20b44D17Dc857d7cf2e0a35Ce',
+  characterAddress: '0x2DCCb7fe1a5FD069202E6FA9cd1545652Ee2e87F',
+  usdtAddress: '0x3813e82e6f7098b9583fc0f33a962d02018b6803',
+};
+
+export default config;
