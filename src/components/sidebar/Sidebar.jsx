4e4c636ba431b2c2037216ca3e481d9220832cb9
nkmcheng <nkmcheng@live.com>
2022-02-18 23:09:51 +0800

    updated sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,8 @@
 /*eslint-disable*/
-import React from "react";
-import PropTypes from "prop-types";
-import { NavLink, useLocation } from "react-router-dom";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink, useLocation } from 'react-router-dom';
+import { HamburgerIcon } from '@chakra-ui/icons';
 // chakra imports
 import {
   Box,
@@ -19,118 +19,112 @@ import {
   useColorModeValue,
   useDisclosure,
   DarkMode,
-} from "@chakra-ui/react";
-import { Separator } from "../separator/Separator";
+  Image,
+} from '@chakra-ui/react';
+import { Separator } from '../separator/Separator';
+import Logo from '../../assets/img/logo.png';
 
-interface SidebarProps {
-  logoText: string;
-  routes: any;
-  sidebarVariant?: string;
-}
-
-function Sidebar(props: SidebarProps) {
+function Sidebar(props) {
   let location = useLocation();
   // this is for the rest of the collapses
   const [state, setState] = React.useState({});
-  let variantChange = "0.2s linear";
+  let variantChange = '0.2s linear';
   // verifies if routeName is the one active (in browser input)
-  const activeRoute = (routeName: string) => {
-    return location.pathname === routeName ? "active" : "";
+  const activeRoute = routeName => {
+    return location.pathname === routeName ? 'active' : '';
   };
   // this function creates the links and collapses that appear in the sidebar (left menu)
-  const createLinks = (routes: any) => {
+  const createLinks = routes => {
     const { sidebarVariant } = props;
     // Chakra Color Mode
-    let activeBg = useColorModeValue("gray.700", "gray.700");
-    let inactiveBg = useColorModeValue("gray.700", "gray.700");
-    let activeColor = useColorModeValue("white", "white");
-    let inactiveColor = useColorModeValue("gray.400", "gray.400");
-    let sidebarActiveShadow = "0px 7px 11px rgba(0, 0, 0, 0.04)";
+    let activeBg = useColorModeValue('gray.700', 'gray.700');
+    let inactiveBg = useColorModeValue('gray.700', 'gray.700');
+    let activeColor = useColorModeValue('white', 'white');
+    let inactiveColor = useColorModeValue('gray.400', 'gray.400');
+    let sidebarActiveShadow = '0px 7px 11px rgba(0, 0, 0, 0.04)';
     // Here are all the props that may change depending on sidebar's state.(Opaque or transparent)
-    if (sidebarVariant === "opaque") {
-      activeBg = "transparent";
-      inactiveBg = useColorModeValue("gray.600", "gray.600");
-      activeColor = useColorModeValue("white", "white");
-      inactiveColor = useColorModeValue("gray.400", "gray.400");
-      sidebarActiveShadow = "none";
+    if (sidebarVariant === 'opaque') {
+      activeBg = 'transparent';
+      inactiveBg = useColorModeValue('gray.600', 'gray.600');
+      activeColor = useColorModeValue('white', 'white');
+      inactiveColor = useColorModeValue('gray.400', 'gray.400');
+      sidebarActiveShadow = 'none';
     }
-    return routes.map((prop: any, key: number) => {
+    return routes.map((prop, key) => {
       return (
-        <DarkMode>
-          <NavLink to={prop.path}>
-            {activeRoute(prop.path) === "active" ? (
-              <Button
-                boxSize="initial"
-                justifyContent="flex-start"
-                alignItems="center"
-                boxShadow={sidebarActiveShadow}
-                bg={activeBg}
-                transition={variantChange}
-                mb={{
-                  xl: "12px",
-                }}
-                mx={{
-                  xl: "auto",
-                }}
-                ps={{
-                  sm: "10px",
-                  xl: "16px",
-                }}
-                py="12px"
-                borderRadius="15px"
-                w="100%"
-                _active={{
-                  bg: "inherit",
-                  transform: "none",
-                  borderColor: "transparent",
-                }}
-                _focus={{
-                  boxShadow: "0px 7px 11px rgba(0, 0, 0, 0.04)",
-                }}
-              >
-                <Flex>
-                  <Text color={activeColor} my="auto" fontSize="sm">
-                    {prop.name}
-                  </Text>
-                </Flex>
-              </Button>
-            ) : (
-              <Button
-                boxSize="initial"
-                justifyContent="flex-start"
-                alignItems="center"
-                bg="transparent"
-                mb={{
-                  xl: "12px",
-                }}
-                mx={{
-                  xl: "auto",
-                }}
-                py="12px"
-                ps={{
-                  sm: "10px",
-                  xl: "16px",
-                }}
-                borderRadius="15px"
-                w="100%"
-                _active={{
-                  bg: "inherit",
-                  transform: "none",
-                  borderColor: "transparent",
-                }}
-                _focus={{
-                  boxShadow: "none",
-                }}
-              >
-                <Flex>
-                  <Text color={inactiveColor} my="auto" fontSize="sm">
-                    {prop.name}
-                  </Text>
-                </Flex>
-              </Button>
-            )}
-          </NavLink>
-        </DarkMode>
+        <NavLink to={prop.path} key={key}>
+          {activeRoute(prop.path) === 'active' ? (
+            <Button
+              boxSize="initial"
+              justifyContent="flex-start"
+              alignItems="center"
+              boxShadow={sidebarActiveShadow}
+              bg={activeBg}
+              transition={variantChange}
+              mb={{
+                xl: '12px',
+              }}
+              mx={{
+                xl: 'auto',
+              }}
+              ps={{
+                sm: '10px',
+                xl: '16px',
+              }}
+              py="12px"
+              borderRadius="15px"
+              w="100%"
+              _active={{
+                bg: 'inherit',
+                transform: 'none',
+                borderColor: 'transparent',
+              }}
+              _focus={{
+                boxShadow: '0px 7px 11px rgba(0, 0, 0, 0.04)',
+              }}
+            >
+              <Flex>
+                <Text color={activeColor} my="auto" fontSize="sm">
+                  {prop.name}
+                </Text>
+              </Flex>
+            </Button>
+          ) : (
+            <Button
+              boxSize="initial"
+              justifyContent="flex-start"
+              alignItems="center"
+              bg="transparent"
+              mb={{
+                xl: '12px',
+              }}
+              mx={{
+                xl: 'auto',
+              }}
+              py="12px"
+              ps={{
+                sm: '10px',
+                xl: '16px',
+              }}
+              borderRadius="15px"
+              w="100%"
+              _active={{
+                bg: 'inherit',
+                transform: 'none',
+                borderColor: 'transparent',
+              }}
+              _focus={{
+                boxShadow: 'none',
+              }}
+            >
+              <Flex>
+                <Text color={inactiveColor} my="auto" fontSize="sm">
+                  {prop.name}
+                </Text>
+              </Flex>
+            </Button>
+          )}
+        </NavLink>
       );
     });
   };
@@ -139,18 +133,18 @@ function Sidebar(props: SidebarProps) {
   var links = <>{createLinks(routes)}</>;
   //  BRAND
   //  Chakra Color Mode
-  const mainText = useColorModeValue("gray.200", "gray.200");
-  let sidebarBg = "none";
-  let sidebarRadius = "0px";
-  let sidebarMargins = "0px";
-  if (sidebarVariant === "opaque") {
-    sidebarBg = useColorModeValue("gray.700", "gray.700");
-    sidebarRadius = "16px";
-    sidebarMargins = "16px 0px 16px 16px";
+  const mainText = useColorModeValue('gray.200', 'gray.200');
+  let sidebarBg = 'none';
+  let sidebarRadius = '0px';
+  let sidebarMargins = '0px';
+  if (sidebarVariant === 'opaque') {
+    sidebarBg = useColorModeValue('gray.700', 'gray.700');
+    sidebarRadius = '16px';
+    sidebarMargins = '16px 0px 16px 16px';
   }
   var brand = (
     <DarkMode>
-      <Box pt={"25px"} mb="12px">
+      <Box pt={'25px'} mb="12px">
         <Link
           href={`${process.env.PUBLIC_URL}/#/`}
           target="_blank"
@@ -176,17 +170,17 @@ function Sidebar(props: SidebarProps) {
   return (
     <DarkMode>
       <Box>
-        <Box display={{ sm: "none", xl: "block" }} position="fixed">
+        <Box display={{ sm: 'none', xl: 'block' }} position="fixed">
           <Box
             bg={sidebarBg}
             transition={variantChange}
             w="260px"
             maxW="260px"
             ms={{
-              sm: "16px",
+              sm: '16px',
             }}
             my={{
-              sm: "16px",
+              sm: '16px',
             }}
             h="calc(100vh - 32px)"
             ps="20px"
@@ -207,54 +201,46 @@ function Sidebar(props: SidebarProps) {
 
 // FUNCTIONS
 
-export function SidebarResponsive(props: SidebarProps) {
-  // to check for active links and opened collapses
+export function SidebarResponsive(props) {
   let location = useLocation();
-  // this is for the rest of the collapses
-  const [state, setState] = React.useState({});
-  const mainPanel = React.useRef();
-  // verifies if routeName is the one active (in browser input)
-  const activeRoute = (routeName: string) => {
-    return location.pathname === routeName ? "active" : "";
+  const activeRoute = routeName => {
+    return location.pathname === routeName ? 'active' : '';
   };
-  // this function creates the links and collapses that appear in the sidebar (left menu)
-  const createLinks = (routes: any) => {
-    // Chakra Color Mode
-    const activeBg = useColorModeValue("gray.700", "gray.700");
-    const inactiveBg = useColorModeValue("gray.700", "gray.700");
-    const activeColor = useColorModeValue("white", "white");
-    const inactiveColor = useColorModeValue("gray.400", "gray.400");
+  const createLinks = routes => {
+    const activeBg = useColorModeValue('gray.700', 'gray.700');
+    const activeColor = useColorModeValue('white', 'white');
+    const inactiveColor = useColorModeValue('gray.400', 'gray.400');
 
-    return routes.map((prop: any, key: number) => {
-      return (
-        <DarkMode>
-          <NavLink to={prop.path}>
-            {activeRoute(prop.path) === "active" ? (
+    return routes.map((prop, key) => {
+      if (prop.showResponsive) {
+        return (
+          <NavLink to={prop.path} key={key}>
+            {activeRoute(prop.path) === 'active' ? (
               <Button
                 boxSize="initial"
                 justifyContent="flex-start"
                 alignItems="center"
                 bg={activeBg}
                 mb={{
-                  xl: "12px",
+                  xl: '12px',
                 }}
                 mx={{
-                  xl: "auto",
+                  xl: 'auto',
                 }}
                 ps={{
-                  sm: "10px",
-                  xl: "16px",
+                  sm: '10px',
+                  xl: '16px',
                 }}
                 py="12px"
                 borderRadius="15px"
                 w="100%"
                 _active={{
-                  bg: "inherit",
-                  transform: "none",
-                  borderColor: "transparent",
+                  bg: 'inherit',
+                  transform: 'none',
+                  borderColor: 'transparent',
                 }}
                 _focus={{
-                  boxShadow: "none",
+                  boxShadow: 'none',
                 }}
               >
                 <Flex>
@@ -270,25 +256,25 @@ export function SidebarResponsive(props: SidebarProps) {
                 alignItems="center"
                 bg="transparent"
                 mb={{
-                  xl: "12px",
+                  xl: '12px',
                 }}
                 mx={{
-                  xl: "auto",
+                  xl: 'auto',
                 }}
                 py="12px"
                 ps={{
-                  sm: "10px",
-                  xl: "16px",
+                  sm: '10px',
+                  xl: '16px',
                 }}
                 borderRadius="15px"
                 w="100%"
                 _active={{
-                  bg: "inherit",
-                  transform: "none",
-                  borderColor: "transparent",
+                  bg: 'inherit',
+                  transform: 'none',
+                  borderColor: 'transparent',
                 }}
                 _focus={{
-                  boxShadow: "none",
+                  boxShadow: 'none',
                 }}
               >
                 <Flex>
@@ -299,8 +285,8 @@ export function SidebarResponsive(props: SidebarProps) {
               </Button>
             )}
           </NavLink>
-        </DarkMode>
-      );
+        );
+      }
     });
   };
   const { logoText, routes, ...rest } = props;
@@ -308,26 +294,25 @@ export function SidebarResponsive(props: SidebarProps) {
   var links = <>{createLinks(routes)}</>;
   //  BRAND
   //  Chakra Color Mode
-  const mainText = useColorModeValue("gray.200", "gray.200");
-  let hamburgerColor = useColorModeValue("gray.200", "gray.200");
+  const mainText = useColorModeValue('gray.200', 'gray.200');
+  let hamburgerColor = useColorModeValue('gray.900', 'gray.200');
   var brand = (
     <DarkMode>
-      <Box pt={"35px"} mb="8px">
-        <Link
-          href={`${process.env.PUBLIC_URL}/#/`}
-          target="_blank"
+      <Box pt={'35px'} mb="8px">
+        <Box
           display="flex"
           lineHeight="100%"
           mb="30px"
           fontWeight="bold"
           justifyContent="center"
           alignItems="center"
-          fontSize="11px"
+          fontSize="12px"
         >
+          <Image src={Logo} boxSize={7} mr={4} />
           <Text fontSize="sm" mt="3px" color={mainText}>
             {logoText}
           </Text>
-        </Link>
+        </Box>
         <Separator />
       </Box>
     </DarkMode>
@@ -339,28 +324,22 @@ export function SidebarResponsive(props: SidebarProps) {
   // Color variables
   return (
     <DarkMode>
-      <Flex display={{ sm: "flex", xl: "none" }} alignItems="center">
-        <HamburgerIcon
-          color={hamburgerColor}
-          w="18px"
-          h="18px"
-          ref={btnRef}
-          onClick={onOpen}
-        />
-        <Drawer isOpen={isOpen} onClose={onClose} placement={"left"}>
+      <Flex display={{ sm: 'flex', xl: 'none' }} alignItems="center">
+        <HamburgerIcon color={hamburgerColor} mt={1} w="19px" h="19px" cursor="pointer" ref={btnRef} onClick={onOpen} />
+        <Drawer isOpen={isOpen} onClose={onClose} placement={'left'}>
           <DrawerOverlay />
           <DrawerContent
             w="250px"
             maxW="250px"
             ms={{
-              sm: "16px",
+              sm: '16px',
             }}
             my={{
-              sm: "16px",
+              sm: '16px',
             }}
             borderRadius="16px"
           >
-            <DrawerCloseButton _focus={{ boxShadow: "none" }} />
+            <DrawerCloseButton _focus={{ boxShadow: 'none' }} />
             <DrawerBody maxW="250px" px="1rem">
               <Box maxW="100%" h="100vh">
                 <Box>{brand}</Box>
