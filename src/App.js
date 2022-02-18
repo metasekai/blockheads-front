e6f259582d3d747a1ec2604e2cda2116b5c52477
nkmcheng <nkmcheng@live.com>
2022-02-18 23:17:16 +0800

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 // Chakra imports
-import React, { useState } from "react";
-import { Portal, useDisclosure } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Portal, useDisclosure } from '@chakra-ui/react';
 
-import Main from "./Main";
-import Footer from "./components/footer/Footer";
-import AdminNavbar from "./components/navbar/AdminNavbar";
-import AuthNavbar from "./components/navbar/AuthNavbar";
-import MainPanel from "./components/layout/MainPanel";
-import PanelContainer from "./components/layout/PanelContainer";
-import PanelContent from "./components/layout/PanelContent";
-import Configurator from "./components/configurator/Configurator";
-import routes from "./routes";
+import Main from './Main';
+import Footer from './components/footer/Footer';
+import AdminNavbar from './components/navbar/AdminNavbar';
+import AuthNavbar from './components/navbar/AuthNavbar';
+import MainPanel from './components/layout/MainPanel';
+import PanelContainer from './components/layout/PanelContainer';
+import PanelContent from './components/layout/PanelContent';
+import routes from './routes';
 
 export default function App(props) {
   const { ...rest } = props;
-  const [fixed, setFixed] = useState(false);
-  const [sidebarVariant, setSidebarVariant] = useState("transparent");
-  const getActiveRoute = (routes) => {
+  const getActiveRoute = routes => {
     for (let i = 0; i < routes.length; i++) {
       if (window.location.href.indexOf(routes[i].path) !== -1) {
         return routes[i];
@@ -25,30 +22,22 @@ export default function App(props) {
   };
 
   const { isOpen, onOpen, onClose } = useDisclosure();
-  document.documentElement.dir = "ltr";
+  document.documentElement.dir = 'ltr';
   // Chakra Color Mode
 
   return (
     <>
       <MainPanel
         w={{
-          base: "100%",
-          xl: "calc(100%)",
+          base: '100%',
+          xl: 'calc(100%)',
         }}
       >
         <Portal>
-          {getActiveRoute(routes).layout === "simple" ? (
-            <AuthNavbar
-              secondary={getActiveRoute(routes).name}
-              logoText="BLOCKHEADS"
-            />
+          {getActiveRoute(routes).layout === 'simple' ? (
+            <AuthNavbar secondary={getActiveRoute(routes).name} logoText="BLOCKHEADS" />
           ) : (
-            <AdminNavbar
-              onOpen={onOpen}
-              logoText={"Dashboard"}
-              brandText={getActiveRoute(routes).name}
-              {...rest}
-            />
+            <AdminNavbar onOpen={onOpen} logoText={'Dashboard'} brandText={getActiveRoute(routes).name} {...rest} />
           )}
         </Portal>
         <PanelContent>
@@ -57,17 +46,6 @@ export default function App(props) {
           </PanelContainer>
         </PanelContent>
         <Footer />
-        <Configurator
-          secondary={false}
-          isOpen={isOpen}
-          onClose={onClose}
-          isChecked={fixed}
-          onSwitch={(value) => {
-            setFixed(value);
-          }}
-          onOpaque={() => setSidebarVariant("opaque")}
-          onTransparent={() => setSidebarVariant("transparent")}
-        />
       </MainPanel>
     </>
   );
