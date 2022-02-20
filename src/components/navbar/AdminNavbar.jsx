3de7e8d58a683a263c0ef05e78994e631c4cf51c
Alleo Indong <alleo.indong@gmail.com>
2022-02-20 16:42:49 +0800

    removed unused variable

--- src/components/navbar/AdminNavbar.jsx
+++ src/components/navbar/AdminNavbar.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { HomeIcon, PersonIcon, RocketIcon } from '../icons/Icons';
