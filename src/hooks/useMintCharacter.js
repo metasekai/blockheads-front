5a5eb4938d9ee5a4fe439868bc8217641fcc33a2
Alleo Indong <alleo.indong@gmail.com>
2022-02-20 19:13:32 +0800

    get the tokenId from logs

--- src/hooks/useMintCharacter.js
+++ src/hooks/useMintCharacter.js
@@ -5,6 +5,25 @@ import { ethers } from 'ethers';
 import { premintABI } from '../artifacts/abi';
 import config from '../artifacts/config';
 
+const NewCharacterMintedTopic = '0x7a24f227bd6b4e20d868408f3e66df824206e3ed7993a2261bd3ff4743d39da0';
+
+const findTopicFromReceipt = receipt => {
+  let topic = [];
+  // find the event topic from all the receipt
+  for (let i = 0; i < receipt.logs.length; i++) {
+    const log = receipt.logs[i];
+    for (let j = 0; j < log.topics.length; j++) {
+      if (log.topics[j] === NewCharacterMintedTopic) {
+        topic = log.topics;
+      }
+    }
+  }
+
+  const tokenId = parseInt(topic[2], 16);
+
+  return tokenId;
+};
+
 const useMintCharacter = () => {
   const [error, setError] = useState(null);
   const auth = useAuth();
@@ -20,7 +39,11 @@ const useMintCharacter = () => {
       const buyTx = await tokenContract.buyNewCharacter(1);
       const receipt = await buyTx.wait();
 
+      console.log(buyTx);
       console.log('receipt', receipt);
+      const eventData = findTopicFromReceipt(receipt);
+
+      console.log(eventData);
       setError(null);
       return true;
     } catch (err) {
